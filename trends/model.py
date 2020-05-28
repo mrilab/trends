6aadc59316ba744862ffc97640a991520177c7dd
Wen-Bin Luo <b00401062@ntu.edu.tw>
2020-05-28 04:41:09 +0000

    Use elastic net

--- trends/model.py
+++ trends/model.py
@@ -1,29 +1,28 @@
+from numpy import (arange, linspace)
 from sklearn.pipeline import make_pipeline
 from sklearn.preprocessing import StandardScaler
 from sklearn.decomposition import PCA
-from sklearn.linear_model import Lasso
+from sklearn.linear_model import ElasticNet
 from sklearn.svm import (SVR, NuSVR)
 from sklearn.kernel_ridge import KernelRidge
 from sklearn.gaussian_process import GaussianProcessRegressor
 from sklearn.model_selection import GridSearchCV
 
 def get_model():
-    pipeline = make_pipeline(
-        PCA(whiten=False),
-        Lasso(),
-    )
-    param_grid = {
-        "pca__n_components": [8, 16, 32, 64, 128, 256, 512],
-        "lasso__alpha": [0.001, 0.01, 0.1, 1, 10, 100, 1000],
-    }
-    metrics = "r2"
-    searcher = GridSearchCV(
-        estimator=pipeline,
-        param_grid=param_grid,
-        scoring=metrics,
+    return GridSearchCV(
+        estimator=make_pipeline(
+            StandardScaler(),
+            PCA(whiten=True),
+            ElasticNet(),
+        ),
+        param_grid={
+            "pca__n_components": arange(0.5, 1, 0.05),
+            "elasticnet__alpha": 4 ** linspace(-5., 5., 11),
+            "elasticnet__l1_ratio": linspace(0, 1, 11),
+        },
+        scoring="r2",
         refit=True,
         cv=5,
         verbose=1,
         n_jobs=-1,
     )
-    return searcher
